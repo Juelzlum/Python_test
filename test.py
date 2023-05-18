845ed28a5fa419c261fd3b6baefe7f9546a8452d
Julian Lum <lumjulian@gmail.com>
2023-05-18 01:09:34 -0700

    practicing python

--- test.py
+++ test.py
@@ -4,4 +4,19 @@ b = 20
 print(a)
 print(b)
 
+my_name = 'Julian Lum'
+print('Hello ' + my_name + '!')
+
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+num1 = input("Enter your num ")
+num2 = input("Enter your num ")
+sum = int(num1) + int(num2)
+print(sum)
 
