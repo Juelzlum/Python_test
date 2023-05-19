8acd1be45b6734994392362762cb398162c862de
Julian Lum <lumjulian@gmail.com>
2023-05-19 00:08:30 -0700

    practice

--- practice.py
+++ practice.py
@@ -3,6 +3,28 @@
 # x += 3 
 # print(x)
 
-x = 3 == 3
+# x = 3 == 3
 
-print(x)
+# print(x)
+
+# temp = 20
+# if temp > 30 :
+#     print('It is hot')
+# elif temp < 30 :
+#     print("Done")
+# else : 
+#     print('It is cold')
+
+
+weight = float(input('Weight: '))
+kg = input("(K)g or (L)bs : ")
+
+if kg.upper() == 'L' : 
+    converted = weight * 2.2
+    print("weight in Lbs:" + str(converted))
+elif kg.upper() == 'K' : 
+     converted = weight / 2.2
+     print("weight in Lbs:" + str(converted))
+
+
+  
