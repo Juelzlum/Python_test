dedf451bb1809b3f1a137b2d78544049fb88356b
Julian Lum <lumjulian@gmail.com>
2023-05-23 00:57:38 -0700

    practice

--- practice.py
+++ practice.py
@@ -47,7 +47,17 @@
 
 # print(math.ceil(2.9))
 
-for x in range(4) : 
-    for y in range (3) : 
-        print(y)
-        print(f'({x}, {y})')
+# for x in range(4) : 
+#     for y in range (3) : 
+#         print(y)
+#         print(f'({x}, {y})')
+
+numbers = [5, 2, 5, 2, 2] 
+
+for item in numbers : 
+    output = '' 
+    for count in range(item) : 
+        output  += 'x'
+    print(output)
+    
+
