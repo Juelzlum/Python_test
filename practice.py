ffad20194818270b94116f23907907810b701156
Julian Lum <lumjulian@gmail.com>
2023-05-23 17:20:41 -0700

    practice

--- practice.py
+++ practice.py
@@ -100,3 +100,14 @@ numbers = [5, 2, 5, 2, 2]
 # print(output)
 
 
+message = input('> ')
+words = message.split(' ') 
+
+emojis = {
+    ':)' : '😃' , 
+    ':(' : '😭'
+}
+output = ''
+for word in words: 
+   output += emojis.get(word, word) + ' '
+print(output)
