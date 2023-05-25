2fc680a03859a1d21ef539058b31f8a3b7fd43fe
Julian Lum <lumjulian@gmail.com>
2023-05-24 23:01:27 -0700

    prac python

--- classes.py
+++ classes.py
@@ -1,47 +1,48 @@
-class Point : 
-    def __init__(self, x, y): 
-        self.x = x 
-        self.y = y
+# class Point : 
+#     def __init__(self, x, y): 
+#         self.x = x 
+#         self.y = y
 
-    def move(self): 
-        print ("move")
+#     def move(self): 
+#         print ("move")
 
-    def draw(self): 
-        print('draw')
+#     def draw(self): 
+#         print('draw')
 
 
-point1 = Point(10, 20 )
-# print(point1.x)
+# point1 = Point(10, 20 )
+# # print(point1.x)
 
-class Person : 
-    def __init__(self, name): 
-        self.name = name
+# class Person : 
+#     def __init__(self, name): 
+#         self.name = name
     
-    def talk(self) : 
-        print(f"Hello I am {self.name}")
+#     def talk(self) : 
+#         print(f"Hello I am {self.name}")
 
-person1 = Person("Julian")
-# print(person1.name)
-person1.talk()
+# person1 = Person("Julian")
+# # print(person1.name)
+# person1.talk()
 
-class Mammal : 
-    def walk(self) : 
-        print("walk")
+# class Mammal : 
+#     def walk(self) : 
+#         print("walk")
 
-class Dog(Mammal) : 
-    pass
+# class Dog(Mammal) : 
+#     pass
 
-class Cat(Mammal) : 
-    pass
+# class Cat(Mammal) : 
+#     pass
 
-dog1 = Dog()
-dog1.walk()
-    
-
-
+# dog1 = Dog()
+# dog1.walk()
     
 
+import converter
+from converter import kgToLbs
 
+kgToLbs(100)
 
+print(converter.kgToLbs(70))
 
 
