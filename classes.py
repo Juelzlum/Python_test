4c63131679639d9e21445119dba940595a7d0e0d
Julian Lum <lumjulian@gmail.com>
2023-05-24 22:55:16 -0700

    practice

---
+++ classes.py
@@ -0,0 +1,47 @@
+class Point : 
+    def __init__(self, x, y): 
+        self.x = x 
+        self.y = y
+
+    def move(self): 
+        print ("move")
+
+    def draw(self): 
+        print('draw')
+
+
+point1 = Point(10, 20 )
+# print(point1.x)
+
+class Person : 
+    def __init__(self, name): 
+        self.name = name
+    
+    def talk(self) : 
+        print(f"Hello I am {self.name}")
+
+person1 = Person("Julian")
+# print(person1.name)
+person1.talk()
+
+class Mammal : 
+    def walk(self) : 
+        print("walk")
+
+class Dog(Mammal) : 
+    pass
+
+class Cat(Mammal) : 
+    pass
+
+dog1 = Dog()
+dog1.walk()
+    
+
+
+    
+
+
+
+
+
